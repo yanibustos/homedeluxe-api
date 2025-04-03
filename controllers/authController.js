3cf4de95f8c9d29b5c1f68ede19a5163e2b79407
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-03 20:19:04 -0300

    authController - Reset password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 async function getToken(req, res) {
   try {
@@ -34,4 +35,68 @@ async function getToken(req, res) {
   }
 }
 
-module.exports = { getToken };
+async function requestPasswordReset(req, res) {
+  const { email } = req.body;
+  console.log("Request received to reset password for email:", email);
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const resetToken = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    const resetLink = `${process.env.API_URL}/reset-password/${resetToken}`;
+    await sendResetEmail(user.email, resetLink);
+
+    res.status(201).json({ msg: "Reset link sent to email" });
+  } catch (error) {
+    console.error("Error during password reset request:", error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+}
+
+async function resetPassword(req, res) {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ where: { email: decoded.email } });
+
+    if (!user) {
+      console.log("User not found or invalid token");
+      return res.status(400).json({ message: "Invalid token or user not found" });
+    }
+    user.password = await bcrypt.hash(password, 10);
+    await user.save();
+
+    res.status(201).json({ msg: "Password reset successful" });
+  } catch (error) {
+    console.error("Error during password reset:", error);
+    return res.status(400).json({ msg: "Invalid or expired token" });
+  }
+}
+
+const sendResetEmail = async (email, resetLink) => {
+  const transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "65a7cc4ab4701a",
+      pass: "de2f4ac8f4afbe",
+    },
+  });
+  console.log("Hola");
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Password Reset Request",
+      html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`,
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Failed to send password reset email");
+  }
+};
+
+module.exports = { getToken, requestPasswordReset, resetPassword };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-
-
 router.post("/tokens", authController.getToken);
-
+router.post("/reset-password", authController.requestPasswordReset);
+router.post("/reset-password/:token", authController.resetPassword);
 
 module.exports = router;
