702d353e5bb2d0fa99d9b0f33e91b850ff8c2f8d
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-03 20:21:04 -0300

    User - Change datatypes due to supabase

--- models/User.js
+++ models/User.js
@@ -1,77 +1,83 @@
 const { sequelize, Model, DataTypes } = require("../setup");
-const Role = require("./Role");
+const bcrypt = require("bcrypt");
 
+class User extends Model {
+  async isValidPassword(password) {
+    try {
+      return await bcrypt.compare(password, this.password);
+    } catch (error) {
+      throw new Error("Password comparison failed");
+    }
+  }
 
+  static async hashedPassword(password) {
+    try {
+      return await bcrypt.hash(password, 10);
+    } catch (error) {
+      throw new Error("Password hashing failed");
+    }
+  }
+}
 
-
-
-class User extends Model { }
 User.init(
-    {
-        firstname: {
-            type: DataTypes.STRING(50),
-            field: 'firstname',
-            allowNull: false
-        },
-        lastname: {
-            type: DataTypes.STRING(50),
-            field: 'lastname',
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING(100),
-            field: 'email',
-            allowNull: false,
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING(100),
-            field: 'password',
-            allowNull: false
-        },
-        phone: {
-            type: DataTypes.INTEGER,
-            field: 'phone'
-        },
-        orders: {
-            type: DataTypes.JSON,
-            field: 'orders',
-            defaultValue: {}
-        },
-        avatar: {
-            type: DataTypes.STRING,
-            field: 'avatar'
-        },
-        country: {
-            type: DataTypes.STRING(50),
-            field: 'country'
-        },
-        neighborhood: {
-            type: DataTypes.STRING(50),
-            field: 'state'
-        },
-        city: {
-            type: DataTypes.STRING(50),
-            field: 'city'
-        },
-        address: {
-            type: DataTypes.TEXT,
-            field: 'address'
-        },
-        zip: {
-            type: DataTypes.TINYINT,
-            field: 'zip'
-        },
-
-
+  {
+    firstname: {
+      type: DataTypes.STRING(50),
+      field: "firstname",
+      allowNull: false,
     },
+    lastname: {
+      type: DataTypes.STRING(50),
+      field: "lastname",
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      field: "email",
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      field: "password",
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      field: "phone",
+    },
+    orders: {
+      type: DataTypes.JSON,
+      field: "orders",
+      defaultValue: {},
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      field: "avatar",
+    },
+    country: {
+      type: DataTypes.STRING(50),
+      field: "country",
+    },
+    neighborhood: {
+      type: DataTypes.STRING(50),
+      field: "state",
+    },
+    city: {
+      type: DataTypes.STRING(50),
+      field: "city",
+    },
+    address: {
+      type: DataTypes.TEXT,
+      field: "address",
+    },
+    zip: {
+      type: DataTypes.INTEGER,
+      field: "zip",
+    },
+  },
 
-
-    { sequelize, modelName: "user" }
+  { sequelize, modelName: "user" },
 );
 
-
-
-
-module.exports = User
-
+module.exports = User;
