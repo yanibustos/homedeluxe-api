09e60f6374e08aacb94d1955c1de09ab7e181e55
Darlen Hornia Puentes <hornia84@gmail.com>
2025-04-08 17:23:31 -0300

    Seeders - Add orderSeeder to runAllSeeders

--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -4,6 +4,7 @@ async function runAllSeeders() {
   await require("./userSeeder")();
   await require("./adminSeeder")();
   await require("./productSeeder")();
+  await require("./orderSeeder")();
 
   console.log("Se corrieron los seeders");
 }
@@ -12,4 +13,3 @@ runAllSeeders();
 
 // para correr los seeders crear primero una base de datos en tableplus, actualizar .env
 // y correr en la terminal "node seeders/runAllSeeders.js"
-
