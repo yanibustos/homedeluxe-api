20651757ed7cf8aa6b74ca0d289f016e53eca652
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-10 13:40:20 -0300

    setup - Remove dialectModule (not neccesary)

--- setup.js
+++ setup.js
@@ -1,19 +1,16 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 
-
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
 
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_CONNECTION,
-        dialectModule: require("pg"),
-        logging: false,
-        port: process.env.DB_PORT
-
-    }
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION,
+    logging: false,
+    port: process.env.DB_PORT,
+  },
 );
 
 module.exports = { sequelize, Model, DataTypes };
