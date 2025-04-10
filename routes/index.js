006c3d95ec340a0001ff3b2ca1b953bdde92797e
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-10 13:45:50 -0300

    pagesRoutes - Remove file and require, not neccesary

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const userRoutes = require("./userRoutes");
 const productRoutes = require("./productRoutes");
 const authRoutes = require("./authRoutes");
-const pagesRoutes = require("./pagesRoutes");
 const orderRoutes = require("./orderRoutes");
 const adminRoutes = require("./adminRoutes");
 const { expressjwt: checkJwt } = require("express-jwt");
@@ -15,5 +14,4 @@ module.exports = (app) => {
   app.use("/users", userRoutes);
   app.use("/orders", orderRoutes);
   app.use("/admin", adminRoutes);
-  app.use("/", pagesRoutes);
 };
