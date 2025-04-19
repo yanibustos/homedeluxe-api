0be03d6bfd75a67e5456469d06b53e075517ef8b
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-19 20:59:07 -0300

    productRoutes - Add protection to CRUD

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const productController = require("../controllers/productController");
 const router = express.Router();
+const { expressjwt: checkJwt } = require("express-jwt");
+const requireAuth = checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] });
 
 router.get("/", productController.index);
 router.get("/featured", productController.showByFeatured);
-router.get("/create", productController.create);
-router.get("/edit/:id", productController.edit);
+router.get("/create", requireAuth, productController.create);
+router.get("/edit/:id", requireAuth, productController.edit);
 router.get("/:id", productController.show);
 
-router.post("/", productController.store);
-router.patch("/:id", productController.update);
-router.delete("/:id", productController.destroy);
-
-// router.get("/categories", productController.getProductCategories);
-// router.get("/category/:category", productController.getProductsInCategory);
+router.post("/", requireAuth, productController.store);
+router.patch("/:id", requireAuth, productController.update);
+router.delete("/:id", requireAuth, productController.destroy);
 
 module.exports = router;
