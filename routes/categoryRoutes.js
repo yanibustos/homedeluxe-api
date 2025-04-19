ea898d468084896f44ac264169124d4b0abe097c
Yanina Bustos <yanibustos4596@gmail.com>
2025-04-19 20:59:23 -0300

    categoryRoutes - Add protection to CRUD

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const categoryController = require("../controllers/categoryController");
 const router = express.Router();
+const { expressjwt: checkJwt } = require("express-jwt");
+const requireAuth = checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] });
 
 router.get("/", categoryController.index);
-router.get("/create", categoryController.create);
-router.post("/", categoryController.store);
+router.get("/create", requireAuth, categoryController.create);
+router.post("/", requireAuth, categoryController.store);
 router.get("/:id", categoryController.show);
-router.get("/edit/:id", categoryController.edit);
-router.patch("/:id", categoryController.update);
-router.delete("/:id", categoryController.destroy);
+router.get("/edit/:id", requireAuth, categoryController.edit);
+router.patch("/:id", requireAuth, categoryController.update);
+router.delete("/:id", requireAuth, categoryController.destroy);
 
 module.exports = router;
